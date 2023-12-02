eed2f64d1d9d18069eaa3ca4ebf758f53eb31e9f
gbenba <gbenba13@gmail.com>
2023-12-02 14:10:34 -0300

    cleaned

--- src/utils/password-hasher.ts
+++ src/utils/password-hasher.ts
@@ -1,15 +1,3 @@
-// import { createHash, randomBytes } from 'node:crypto';
-
-// const getSalt = () => randomBytes(18).toString('hex');
-
-// const getSHA512OfPassword = (password: string, salt: string) =>
-// 	createHash('sha512')
-// 		.update(salt + password)
-// 		.digest('hex');
-
-// export { getSHA512OfPassword, getSalt };
-
-
 import { createHash, randomBytes } from 'node:crypto';
 import PEPPER from './pepper'
 
